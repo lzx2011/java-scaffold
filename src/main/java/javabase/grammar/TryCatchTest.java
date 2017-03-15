c2964235dcf4d20d35d5e47e5738cd774b285ff3
gary.liu <lzx20110@163.com>
2017-03-15 21:10:00 +0800

    process practice

--- src/main/java/javabase/grammar/TryCatchTest.java
+++ src/main/java/javabase/grammar/TryCatchTest.java
@@ -23,9 +23,21 @@ public class TryCatchTest {
 		}
 	}
 
-	public static void main(String[] args) {
-		String msg = "sdfs";
-		System.out.println(TryCatchTest.testReturn());
+	//public static void main(String[] args) {
+	//	String msg = "sdfs";
+	//	System.out.println(TryCatchTest.testReturn());
+    //
+	//}
 
+	public static void main(String[] args) {
+		//System.out.println("OUTPUT one");
+		//System.out.println("OUTPUT two");
+		//System.err.println("ERROR 1");
+		//System.err.println("ERROR 2");
+		for(int i = 0; i < args.length; i++)
+		{
+			System.out.printf("args[%d] = %s.", i, args[i]);
+		}
 	}
 }
+
