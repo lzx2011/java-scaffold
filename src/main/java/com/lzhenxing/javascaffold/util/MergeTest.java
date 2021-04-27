23d5655bd3ed3d448df2b34172d3209252a8760b
aiden.liu <aiden.liu@zoom.us>
2021-04-27 14:31:59 +0800

    add test1

--- src/main/java/com/lzhenxing/javascaffold/util/MergeTest.java
+++ src/main/java/com/lzhenxing/javascaffold/util/MergeTest.java
@@ -1,5 +1,9 @@
 package com.lzhenxing.javascaffold.util;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class MergeTest {
     private String name;
+    private String test1;
 }
